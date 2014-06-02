71952b9c1e00e2901fc669125bf5738c68e50048
vikdenic <vik.denic@gmail.com>
2014-06-02 01:34:29 -0500

    Initial commit. Customized UITabBar in AppDelegate

---
+++ BigCatPhotoBrowser/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BigCatPhotoBrowser
+//
+//  Created by Vik Denic on 6/1/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+@end
